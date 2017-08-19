c1fbafccf66213f1c5e29a6483c41a87bd0147cd
thanawat <tha_chookes@hotmail.com>
2017-08-19 22:02:38 +0700

    youtube search

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import Youtube from 'react-youtube'
-import { Row, Col, Input, Form, message } from 'antd'
-
+import { Row, Col, Input, Form, message, Button } from 'antd'
+import Axios from 'axios'
 import socket from './config'
 
 const { Search } = Input
 class App extends Component {
   state = {
     list: '',
-    lists: []
+    lists: [],
+    showPlaylist: true,
+    searchs: []
   }
   componentDidMount = () => {
-    console.log('test')
     socket.on('newVdo', (data) => {
-      console.log(data)
       this.setState({
         lists: [ ...this.state.lists, data ]
       })
@@ -30,24 +30,28 @@ class App extends Component {
     })
   }
   handleClick = (value) => {
-    // console.log(this.state.list)
-    let { lists } = this.state
-    if (lists.filter(item => item.link=== value).length === 0) {
-      socket.emit('newVdo', {link: value})
+    Axios.get(`http://localhost:7001/search?keyword=${value}`).then((res) => {
+      console.log(res.data)
       this.setState({
-        lists: [ ...this.state.lists, {link: value} ],
-        list: ''
+        showPlaylist: false,
+        searchs: res.data.items
       })
-      message.success('Added to playlist')
-    } else {
-      message.error('This link is already added')
-    }
+    })
   }
   handleRemove = (index) => {
     let lists = this.state.lists
     lists.splice(index, 1)
     this.setState({ lists})
     socket.emit('newLists', { lists})
+  }
+  handleAdd = (id, title, img) => {
+    // console.log(id,title,img)
+    let list = { id, title, img }
+    console.log(list)
+    this.setState({
+      showPlaylist: true,
+      lists: [ ...this.state.lists, list ]
+    })
   }
   endVdo = () => {
     let lists = this.state.lists
@@ -56,7 +60,7 @@ class App extends Component {
     socket.emit('newLists', { lists})
   }
   render () {
-    const { lists } = this.state
+    const { lists, showPlaylist, searchs } = this.state
     const opts = {
       height: '390',
       width: '640',
@@ -68,20 +72,37 @@ class App extends Component {
       <div style={{marginTop: '30px', marginRight: '10px'}}>
         <Row>
           <Col xs={16} style={{textAlign: 'center'}}>
-          {!this.state.lists[0] ?
+          {!lists[0] ?
              <h1>Please Enter Link</h1> :
-             <Youtube videoId={this.state.lists[0].link.split('=')[1]} opts={opts} onEnd={this.endVdo} />}
+             <Youtube videoId={lists[0].id} opts={opts} onEnd={this.endVdo} />}
           </Col>
           <Col xs={8}>
           <Search onSearch={value => this.handleClick(value)} />
           <div>
             {
-              lists.map((item, index) => (
-                <div key={item.link+index}>
-                  <span> {item.link} </span>
-                  <a onClick={() => this.handleRemove(index)}>Remove</a>
+              showPlaylist ?
+              lists.map((list, index) => (
+                <div key={index} style={{display: 'flex', marginTop: '10px'}} >
+                  <div>
+                    <img src={list.img} alt="" style={{width:'250px'}} />
+                  </div>
+                  <div style={{display: 'flex', flexDirection:'column', justifyContent:'space-evenly', width: '100%', textAlign:'center', marginLeft: '8px'}}>
+                    <div>{list.title}</div>
+                     <Button type="primary" onClick={ () => this.handleRemove(index) } >REMOVE</Button>
+                  </div>
                 </div>
-              ))
+              )):
+                searchs.map((search, index) => (
+                  <div key={index} style={{display: 'flex', marginTop: '10px'}} >
+                    <div>
+                      <img src={search.snippet.thumbnails.medium.url} alt="" style={{width:'250px'}} />
+                    </div>
+                    <div style={{display: 'flex', flexDirection:'column', justifyContent:'space-evenly', width: '100%', textAlign:'center', marginLeft: '8px'}}>
+                      <div>{search.snippet.title}</div>
+                       <Button type="primary" onClick={ () => this.handleAdd(search.id.videoId, search.snippet.title, search.snippet.thumbnails.medium.url) } >ADD</Button>
+                    </div>
+                  </div>
+                ))
             }
           </div>
           </Col>
