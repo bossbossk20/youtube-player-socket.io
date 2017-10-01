780d9c9aa4cb5e81eb92e10e91a53e3d98e0af1a
mdew192837 <justin.d128@gmail.com>
2017-10-01 08:51:58 -0500

    Don't use react-router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Router from 'react-router'
-import routes from './routes'
 import ReactGA from 'react-ga'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
@@ -10,11 +8,5 @@ import 'antd/dist/antd.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-ReactGA.initialize('UA-101263186-4');
-function logPageView() {
-  ReactGA.set({ page: window.location.pathname + window.location.search });
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
-var app = document.getElementById('app');
-ReactDOM.render(<Router routes={routes} onUpdate={logPageView} />, app);
+ReactGA.initialize('UA-101263186-4', { debug: true });
 registerServiceWorker()
