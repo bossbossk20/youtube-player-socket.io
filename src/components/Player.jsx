f7555114cfeec913d43a5fe1c4290873e812395a
AzrizHaziq <haziq@nazrol.tech>
2017-10-01 15:09:58 +0800

    add eslint
    -fix all to js Standart

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,22 +2,26 @@ import Youtube from 'react-youtube'
 import React, { Component } from 'react'
 
 const opts = {
-    height: '390',
-    width: '640',
-    playerVars: {
-        autoplay: 1
-    }
+  height: '390',
+  width: '640',
+  playerVars: {
+    autoplay: 1
+  }
 }
 
 class Player extends Component {
+  render () {
+    const {lists, endVdo} = this.props
 
-    render () {
-        const { lists, endVdo } = this.props
+    return !lists
+      ? <h1>Please Enter Link</h1>
+      : <Youtube videoId={lists[0].id} opts={opts} onEnd={endVdo} />
+  }
+}
 
-        return !lists
-            ? <h1>Please Enter Link</h1>
-            : <Youtube videoId={lists[0].id} opts={opts} onEnd={endVdo} />
-    }
+Player.propTypes = {
+  lists: React.PropTypes.array,
+  endVdo: React.PropTypes.func
 }
 
 export default Player
--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -2,59 +2,79 @@ import { Button } from 'antd'
 import React, { Component } from 'react'
 
 class PlayerList extends Component {
+  removePlayList (id) {
+    this.props.handleRemove(id)
+  }
 
-    removePlayList = (id) => {
-        this.props.handleRemove(id)
-    }
+  addPlayList (search) {
+    const videoId = search.id.videoId
+    const title = search.snippet.title
+    const thumbnailUrl = search.snippet.thumbnails.medium.url
 
-    addPlayList = (search) => {
-        const videoId = search.id.videoId
-        const title = search.snippet.title
-        const thumbnailUrl = search.snippet.thumbnails.medium.url
+    this.props.handleAdd(videoId, title, thumbnailUrl)
+  }
 
-        this.props.handleAdd(videoId, title, thumbnailUrl)
-    }
+  render () {
+    const {showPlaylist, lists, searches} = this.props
 
-    render () {
-        const { showPlaylist, lists, searches } = this.props
+    return <div>
+      {
+        showPlaylist
+          ? (
+            lists.map((list, index) => (
+              <div key={index} style={{display: 'flex', marginTop: '10px'}}>
+                <div>
+                  <img src={list.img} alt='' style={{width: '250px'}} />
+                </div>
+                <div style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-evenly',
+                  width: '100%',
+                  textAlign: 'center',
+                  marginLeft: '8px'
+                }}>
+                  <div>{ list.title }</div>
+                  <Button type='primary' onClick={() => this.removePlayList()}>REMOVE</Button>
+                </div>
+              </div>
+            ))
+          )
 
-        return <div>
-            {
-                showPlaylist
-                    ? (
-                        lists.map((list, index) => (
-                            <div key={index} style={{display: 'flex', marginTop: '10px'}} >
-                                <div>
-                                    <img src={list.img} alt="" style={{width:'250px'}} />
-                                </div>
-                                <div style={{display: 'flex', flexDirection:'column', justifyContent:'space-evenly', width: '100%', textAlign:'center', marginLeft: '8px'}}>
-                                    <div>{list.title}</div>
-                                    <Button type="primary" onClick={ () => this.removePlayList() } >REMOVE</Button>
-                                </div>
-                            </div>
-                        ))
-                    )
+          : (
+            searches.map((search, index) => (
+              <div key={index} style={{display: 'flex', marginTop: '10px'}}>
+                <div>
+                  <img src={search.snippet.thumbnails.medium.url} alt='' style={{width: '250px'}} />
+                </div>
+                <div style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'space-evenly',
+                  width: '100%',
+                  textAlign: 'center',
+                  marginLeft: '8px'
+                }}>
+                  <div>{ search.snippet.title }</div>
+                  <Button type='primary' onClick={() => this.addPlayList(search)}>
+                    ADD
+                  </Button>
+                </div>
+              </div>
+            ))
+          )
 
-                    : (
-                        searches.map((search, index) => (
-                            <div key={index} style={{display: 'flex', marginTop: '10px'}} >
-                                <div>
-                                    <img src={search.snippet.thumbnails.medium.url} alt="" style={{width:'250px'}} />
-                                </div>
-                                <div style={{display: 'flex', flexDirection:'column', justifyContent:'space-evenly', width: '100%', textAlign:'center', marginLeft: '8px'}}>
-                                    <div>{search.snippet.title}</div>
-                                    <Button type="primary"
-                                            onClick={ () => this.addPlayList(search)}>
-                                        ADD
-                                    </Button>
-                                </div>
-                            </div>
-                        ))
-                    )
+      }
+    </div>
+  }
+}
 
-            }
-        </div>
-    }
+PlayerList.propTypes = {
+  showPlaylist: React.PropTypes.bool,
+  lists: React.PropTypes.array,
+  searches: React.PropTypes.array,
+  handleRemove: React.PropTypes.func,
+  handleAdd: React.PropTypes.func
 }
 
 export default PlayerList
