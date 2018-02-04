0561cb786ac49a25bffb8b397c56ad6079568d91
thanawat <tha_chookes@hotmail.com>
2018-02-04 22:00:18 +0700

    change api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
 
-const uri = 'https://api.kody-koy.me'
+const uri = 'https://api-music.herokuapp.com'
 export const socket = io.connect(uri)
 export const URI = uri
